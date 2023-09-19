ca8ef16852db06f6f3722f3be3d6df71d4b0b1da
Abdallah Emad <Abdallah.coder@gmail.com>
2023-09-19 23:52:31 +0200

    - use lombok to reduce the code boilerplate

--- src/main/java/de/db/product/tradingapplication/controller/SignalRequest.java
+++ src/main/java/de/db/product/tradingapplication/controller/SignalRequest.java
@@ -1,5 +1,10 @@
 package de.db.product.tradingapplication.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 /**
  * Represents a trading signal request.
  * This class uses the builder pattern for constructing instances.
@@ -7,55 +12,11 @@ package de.db.product.tradingapplication.controller;
  * @author Abdallah Emad.
  * @since 19-9-2023
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SignalRequest {
-  private final int signal;
-
-  private SignalRequest(Builder builder) {
-    this.signal = builder.signal;
-  }
-
-  /**
-   * Get the trading signal value.
-   *
-   * @return The trading signal value.
-   */
-  public int getSignal() {
-    return signal;
-  }
-
-  /**
-   * Builder class for constructing SignalRequest instances.
-   */
-  public static class Builder {
-    private int signal;
-
-    /**
-     * Set the trading signal value.
-     *
-     * @param signal The trading signal value.
-     * @return The builder instance.
-     */
-    public Builder signal(int signal) {
-      this.signal = signal;
-      return this;
-    }
-
-    /**
-     * Build a SignalRequest instance.
-     *
-     * @return The constructed SignalRequest.
-     */
-    public SignalRequest build() {
-      return new SignalRequest(this);
-    }
-  }
-
-  /**
-   * Create a new builder instance for SignalRequest.
-   *
-   * @return A new builder instance.
-   */
-  public static Builder builder() {
-    return new Builder();
-  }
+  private int signal;
 }
